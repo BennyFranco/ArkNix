cda47eb89a9eb63da1c2d18ee18bb5058dd7fc9b
Benny Franco <bennyfranco@outlook.com>
2022-08-13 20:42:52 -0700

    Use cpp for renderer locator

--- engine/graphics/renderer_locator.cpp
+++ engine/graphics/renderer_locator.cpp
@@ -1,3 +1,8 @@
 #include "renderer_locator.h"
 
 std::unique_ptr<Renderer> RendererLocator::renderer;
+
+void RendererLocator::Initialize() {
+    renderer = std::make_unique<SDLRenderer>();
+    renderer->Init();
+}
--- engine/graphics/renderer_locator.h
+++ engine/graphics/renderer_locator.h
@@ -1,17 +1,14 @@
 #ifndef RENDERER_SERVICE_LOCATOR_H
 #define RENDERER_SERVICE_LOCATOR_H
 
-#include <memory>
 #include "renderer.h"
 #include "sdl_renderer.h"
+#include <memory>
 
 class RendererLocator {
 public:
     static Renderer *GetRenderer() { return renderer.get(); }
-    static void Provide() {
-        renderer = std::make_unique<SDLRenderer>();
-        renderer->Init();
-    }
+    static void Initialize();
 
 private:
     static std::unique_ptr<Renderer> renderer;
