0f4a0382ca5e683a5bdf81e962b2292a8b922a11
Benny Franco <bennyfranco@outlook.com>
2022-08-17 14:19:48 -0700

    Remove deprecated class

--- engine/input/keyboard.cpp
+++
@@ -1,9 +0,0 @@
-#include "keyboard.h"
-
-bool Keyboard::GetKeyDown(Key key) {
-    return InputLocator::GetInput()->GetKeyDown(key);
-}
-
-bool Keyboard::GetKeyUp(Key key) {
-    return InputLocator::GetInput()->GetKeyUp(key);
-}
