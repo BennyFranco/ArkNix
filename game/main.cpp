c36810141602bcddf1f4177bd06945e0b650a0c8
Benny Franco <bennyfranco@outlook.com>
2022-08-18 07:09:37 -0700

    Test code of main

--- game/main.cpp
+++ game/main.cpp
@@ -1,8 +1,23 @@
-
 #include "galaga.h"
+#include "scene.h"
+#include "sprite.h"
 
 int main(int argc, char **args) {
     Galaga newGame;
+    newGame.Awake();
+
+    // TEST CODE
+    nim::GameObject player("Player");
+    nim::Sprite *sprite = nim::AssetManager::Instance().Get<nim::Sprite>("ship_6");
+    player.AddComponent("texture", sprite);
+    player.transform->Size(128, 128);
+    player.transform->Position(sprite->Centered());
+
+    auto currentScene = std::make_unique<nim::Scene>();
+    currentScene->Init();
+    currentScene->sceneData->gameObjects.emplace_back(std::move(player));
+
+    newGame.currentScene = std::move(currentScene);
     newGame.Run();
     return 0;
 }
