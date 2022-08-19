14f91fcb895df91f4be25601f86ba8d02a110149
Benny Franco <bennyfranco@outlook.com>
2022-08-18 17:48:08 -0700

    Encode/Decode vector2 yaml

--- engine/math/vector2d.h
+++ engine/math/vector2d.h
@@ -1,36 +1,60 @@
 #ifndef VECTOR2D_H
 #define VECTOR2D_H
 
-struct Vector2 {
-    float x;
-    float y;
+#include "yaml-cpp/yaml.h"
 
-    Vector2() {
-        x = 0.f;
-        y = 0.f;
-    }
+namespace nim {
+    struct Vector2 {
+        float x;
+        float y;
 
-    Vector2(float a, float b) {
-        x = a;
-        y = b;
-    }
+        Vector2() {
+            x = 0.f;
+            y = 0.f;
+        }
 
-    Vector2 &operator=(float value) {
-        x = value;
-        y = value;
-        return *this;
-    }
+        Vector2(float a, float b) {
+            x = a;
+            y = b;
+        }
 
-    Vector2 &operator=(Vector2 value) {
-        x = value.x;
-        y = value.y;
-        return *this;
-    }
+        Vector2 &operator=(float value) {
+            x = value;
+            y = value;
+            return *this;
+        }
 
-    float w() const { return x; }
-    float h() const { return y; }
-    void w(float value) { x = value; }
-    void h(float value) { y = value; }
-};
+        Vector2 &operator=(Vector2 value) {
+            x = value.x;
+            y = value.y;
+            return *this;
+        }
+
+        float w() const { return x; }
+        float h() const { return y; }
+        void w(float value) { x = value; }
+        void h(float value) { y = value; }
+    };
+}// namespace nim
+
+namespace YAML {
+    template<>
+    struct convert<nim::Vector2> {
+        static YAML::Node encode(const nim::Vector2 &vector) {
+            YAML::Node node;
+            node.SetStyle(YAML::EmitterStyle::Flow);
+            node.push_back(vector.x);
+            node.push_back(vector.y);
+            return node;
+        }
+
+        static bool decode(const YAML::Node &node, nim::Vector2 &vector) {
+            if (!node.IsSequence() || node.size() != 2) return false;
+            vector.x = node[0].as<float>();
+            vector.y = node[1].as<float>();
+            return true;
+        }
+    };
+}// namespace YAML
 
 #endif
