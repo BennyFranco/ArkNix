f8b6e741502032eaf451565b8593d7061caa46ae
Benny Franco <bennyfranco@outlook.com>
2022-08-23 14:41:38 -0700

    agnostic way to convert path into string

--- engine/core/asset_manager/asset_manager.cpp
+++ engine/core/asset_manager/asset_manager.cpp
@@ -20,8 +20,8 @@ void AssetManager::LoadAssets() {
         if (!item.is_regular_file()) continue;
         // TODO: define more supported formats
         if (item.path().extension() == ".png") {
-            std::string filename = item.path().c_str();
-            std::string id = item.path().filename().replace_extension("").c_str();
+            std::string filename = item.path().string();
+            std::string id = item.path().filename().replace_extension("").string();
             assets.emplace(id, std::make_shared<nim::Sprite>(id.c_str(), filename.c_str()));
             std::cout << "[AssetManager] Asset loaded with id = " << id << "\n";
         }
