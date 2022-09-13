7c86fd2adcdc683a884413cd40591f81110eecef
Benny Franco Dennis <bennyfranco@outlook.com>
2022-09-12 18:52:20 -0700

    Remove entity inheritance, we don't need it anymore

--- engine/graphics/renderer.h
+++ engine/graphics/renderer.h
@@ -9,8 +9,10 @@
 #include "vector2d.h"
 
 namespace nim {
-    class Renderer : public Entity {
+    class Renderer {
     public:
+        virtual ~Renderer() = default;
+
         virtual void CreateWindow(std::string title, int x_position,
                                   int y_position, int width, int height, uint flags) = 0;
         virtual void CreateWindow(std::string title, int width, int height) = 0;
@@ -20,6 +22,10 @@ namespace nim {
         virtual int Width() = 0;
         virtual int Height() = 0;
         virtual Vector2int WindowSize() = 0;
+
+        virtual void Init() = 0;
+        virtual void Update() = 0;
+        virtual void Quit() = 0;
     };
 }// namespace nim
 #endif// RENDERER_H
